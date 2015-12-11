21a496cd795c1641776857bd433bf3348d08217e
Walker Spurgeon <axesftw@hotmail.com>
2015-12-10 16:23:51 -0800

    no message

--- HCI Project/Assets/Scripts/Card.cs
+++ HCI Project/Assets/Scripts/Card.cs
@@ -2,17 +2,13 @@
 using System.Collections;
 
 
-public class Card : MonoBehaviour 
+public abstract class Card : MonoBehaviour 
 {
 
 	public string CardColor;
-	public string SecondColor;
 	public string CardName;
 	public string CardType;
-	public string SecondCardType;
 	public string CardSubType;
-	public string SecondCardSubType;
-
 	public int TotalCost;
 	public int RedCost;
 	public int BlueCost;
@@ -20,20 +16,7 @@ public class Card : MonoBehaviour
 	public int WhiteCost;
 	public int BlackCost;
 	public int ColorlessCost;
-
-
-	// May make effects functions
-	public string Effect1;
-	public string Effect2;
-	public string Effect3;
-
-	public int power;
-	public int toughness;
 	public bool tapped;
 
-
-	public Card()
-	{
-	}
-
+	public abstract void effect1();
 }
