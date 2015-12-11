6df05c336da7965b8274e377218a09cce66d6cb3
Tanvir Ahmed <tanvir.mclv@gmail.com>
2015-12-10 17:42:22 -0800

    test

---
+++ HCI Project/Assets/Scripts/goblinRoughrider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goblinRoughrider : Card {
+
+    public int power;
+    public int toughness;
+
+    public goblinRoughrider()
+    {
+        power = 3;
+        toughness = 2;
+        CardColor = "Red";
+        CardName = "Goblin Roughrider";
+        CardType = "Creature";
+        CardSubType = "Goblin Knight";
+        TotalCost = 3;
+        RedCost = 1;
+        BlueCost = 0;
+        GreenCost = 0;
+        WhiteCost = 0;
+        BlackCost = 0;
+        ColorlessCost = 2;
+        tapped = true;
+    }
+
+    public override void effect1();
+
+}
---
+++ HCI Project/Assets/Scripts/mountain.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mountain : Card {
+
+    public mountain() {
+
+        CardColor = "Red";
+        CardName = "Mountain";
+        CardType = "Land" ;
+        CardSubType = "Mountain";
+        TotalCost = 0;
+        RedCost = 0;
+        BlueCost =0;
+        GreenCost = 0;
+        WhiteCost = 0;
+        BlackCost = 0;
+        ColorlessCost = 0;
+        tapped = false;
+}
+    public override void effect1();
+    
+}
